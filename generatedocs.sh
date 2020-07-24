27bca2868163f7de723845aac436dc66c3f44006
Göran Krampe <goran.krampe@gmail.com>
2020-07-24 14:59:59 +0200

    Removed exact-match from git describe

--- generatedocs.sh
+++ generatedocs.sh
@@ -5,7 +5,7 @@ then
   echo "Need at least Nim version 1.3.x for this to work"
   exit 1
 fi
-TAG=`git describe --exact-match --tags`
+TAG=`git describe --tags`
 echo Generating documentation for commit/tag: $TAG
 rm -rf htmldocs/*
 nim doc --project --outdir:htmldocs --index:on --git.url:https://github.com/gokr/norx --git.commit:$TAG src/norx.nim
