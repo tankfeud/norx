561a865ac3ac472d4e29b4692601b346fa69c4c6
jseb <git@finiderire.com>
2020-07-27 20:22:31 +0200

    S02 finished!

--- official_samples/S02_clock.nim
+++ official_samples/S02_clock.nim
@@ -40,7 +40,7 @@
 ]#
 
 import strformat
-import norx, norx/[incl, config, viewport, obj, input, keyboard, clock]
+import norx, norx/[incl, config, viewport, obj, input, keyboard, clock, math]
 
 
 
@@ -77,49 +77,63 @@ proc getBinding*(
 #proc Update( clock_info:orxCLOCK_FUNCTION, context:pointer) =
 # 
 proc Update( clock_info:ptr orxCLOCK_INFO, context:pointer) {.cdecl.} =
+  #### LOG DISPLAY SECTION ####
   # push Main on section stack for accessing informations related to this section.
   var status = pushSection("Main")
 
   if getBool("DisplayLog"):
-    echo(getName( getFromInfo( clock_info)))
+    # clock.getFromInfo( :ptr orxCLOCK_INFO): ptr orxCLOCK
+    # clock.getName : returns the orxCLOCK name as a cstring
+    # cast , convert, use echo directly, or $ which convert cstring to string (safe but slow)
+    let clock_name:string = $getName( getFromInfo( clock_info) )
+    orxLOG( &"clock_name time:{clock_info.fTime:3.2f} delta:{clock_info.fDT:1.4f}")
 
+  # pop "Main" , still no test on orxStatus.
+  status = popSection();
 
-#[
-void orxFASTCALL Update(const orxCLOCK_INFO *_pstClockInfo, void *_pstContext)
-{
-  orxOBJECT *pstObject;
+  #### OBJECT UPDATE SECTION ####
+  # gets object from the context
+  var obj :ptr orxOBJECT = cast[ptr orxOBJECT]( context)
+  
+  # Rotates it according to elapsed time (complete rotation every 2 seconds)
+  status = setRotation( obj, orxMATH_KF_PI * clock_info.fTime);
 
-  /* *** LOG DISPLAY SECTION *** */
-
-  /* Pushes main config section */
-  orxConfig_PushSection("Main");
-
-  /* Should display log? */
-  if(orxConfig_GetBool("DisplayLog"))
-  {
-    /* Displays info in log and console */
-    orxLOG("<%s>: Time = %.3f / DT = %.3f", orxClock_GetName(orxClock_GetFromInfo(_pstClockInfo)), _pstClockInfo->fTime, _pstClockInfo->fDT);
-  }
-
-  /* Pops config section */
-  orxConfig_PopSection();
-
-  /* *** OBJECT UPDATE SECTION *** */
-
-  /* Gets object from context.
-   * The helper macro orxOBJECT() will verify if the pointer we want to cast
-   * is really an orxOBJECT.
-   */
-  pstObject = orxOBJECT(_pstContext);
-
-  /* Rotates it according to elapsed time (complete rotation every 2 seconds) */
-  orxObject_SetRotation(pstObject, orxMATH_KF_PI * _pstClockInfo->fTime);
-}
-]#
 
 proc inputUpdate( clock_info:ptr orxCLOCK_INFO, context:pointer) {.cdecl.} =
-  # the input management is called by the main clock
-  discard
+  # note : the ESC test for exiting is called by the main clock in " run " proc.
+  
+  #### LOG DISPLAY SECTION ####
+  # push Main on section stack for accessing informations related to this section.
+  var status = pushSection("Main")
+  
+  # Is "Log" key has been pressed ?
+  if hasBeenActivated("Log"):
+    # Toggles logging on / off
+    status = setBool("DisplayLog", not getBool("DisplayLog") )
+
+  # Pops config section
+  status = popSection()
+
+
+  #### CLOCK TIME STRETCHING SECTION ####
+  # Finds clock1.
+  # We could have stored the clock at creation, of course, but this is done here for didactic purpose.
+  var clock1:ptr orxCLOCK = clockGet("Clock1");
+
+  # this time, we test keyboard only if we could get the first Clock (which makes turning the box)
+  if clock1 != nil:
+    if isActive( "Faster"):
+      # Makes this clock go four time faster
+      status = setModifier( clock1, orxCLOCK_MOD_TYPE_MULTIPLY, orx2F(4.0f))
+      
+    elif isActive( "Slower"):
+      # Makes this clock go four time slower
+      status = setModifier( clock1, orxCLOCK_MOD_TYPE_MULTIPLY, orx2F(0.25f))
+
+    elif isActive( "Normal"):
+      # Removes modifier from this clock
+      status = setModifier( clock1, orxCLOCK_MOD_TYPE_NONE, orxFLOAT_0)
+
 
 proc init(): orxSTATUS {.cdecl.} =
   result = orxSTATUS_SUCCESS
@@ -162,7 +176,6 @@ proc init(): orxSTATUS {.cdecl.} =
   #[ Registers our update callback to these clocks with both object as context.
      The module ID is used to skip the call to this callback if the corresponding module
      is either not loaded or paused, which won't happen in this tutorial. ]#
-#  register( clock1, Update, object1, orxMODULE_ID_MAIN, orxCLOCK_PRIORITY_NORMAL)
   var status = register( clock1, Update, object1, orxMODULE_ID_MAIN, orxCLOCK_PRIORITY_NORMAL)
   # you can also call register with clock2 as first implicit parameter.
   status = clock2.register( Update, object2, orxMODULE_ID_MAIN, orxCLOCK_PRIORITY_NORMAL)
@@ -185,8 +198,6 @@ proc run(): orxSTATUS {.cdecl.} =
     result = orxSTATUS_FAILURE
 
 
-
-
 proc exit() {.cdecl.} =
   # We're a bit lazy here so we let orx clean all our mess! :)
   quit(0)
@@ -203,192 +214,3 @@ proc Main =
 
 Main()
 
-
-#[
-
-/** Inits the tutorial
- */
-orxSTATUS orxFASTCALL Init()
-{
-  orxCLOCK       *pstClock1, *pstClock2, *pstMainClock;
-  orxOBJECT      *pstObject1, *pstObject2;
-  orxINPUT_TYPE   eType;
-  orxENUM         eID;
-  orxINPUT_MODE   eMode;
-  const orxSTRING zInputLog;
-  const orxSTRING zInputFaster;
-  const orxSTRING zInputSlower;
-  const orxSTRING zInputNormal;
-
-  /* Gets input binding names */
-  orxInput_GetBinding("Log", 0, &eType, &eID, &eMode);
-  zInputLog     = orxInput_GetBindingName(eType, eID, eMode);
-
-  orxInput_GetBinding("Faster", 0, &eType, &eID, &eMode);
-  zInputFaster  = orxInput_GetBindingName(eType, eID, eMode);
-
-  orxInput_GetBinding("Slower", 0, &eType, &eID, &eMode);
-  zInputSlower  = orxInput_GetBindingName(eType, eID, eMode);
-
-  orxInput_GetBinding("Normal", 0, &eType, &eID, &eMode);
-  zInputNormal  = orxInput_GetBindingName(eType, eID, eMode);
-
-  /* Displays a small hint in console */
-  orxLOG("\n- Press '%s' to toggle log display"
-         "\n- To stretch time for the first clock (updating the box):"
-         "\n . Press numpad '%s' to set it 4 times faster"
-         "\n . Press numpad '%s' to set it 4 times slower"
-         "\n . Press numpad '%s' to set it back to normal", zInputLog,  zInputFaster, zInputSlower, zInputNormal);
-
-  /* Creates viewport */
-  orxViewport_CreateFromConfig("Viewport");
-
-  /* Creates objects */
-  pstObject1 = orxObject_CreateFromConfig("Object1");
-  pstObject2 = orxObject_CreateFromConfig("Object2");
-
-  /* Creates two user clocks: a 100Hz and a 5Hz */
-  pstClock1 = orxClock_CreateFromConfig("Clock1");
-  pstClock2 = orxClock_CreateFromConfig("Clock2");
-
-  /* Registers our update callback to these clocks with both object as context.
-   * The module ID is used to skip the call to this callback if the corresponding module
-   * is either not loaded or paused, which won't happen in this tutorial.
-   */
-  orxClock_Register(pstClock1, Update, pstObject1, orxMODULE_ID_MAIN, orxCLOCK_PRIORITY_NORMAL);
-  orxClock_Register(pstClock2, Update, pstObject2, orxMODULE_ID_MAIN, orxCLOCK_PRIORITY_NORMAL);
-
-  /* Gets main clock */
-  pstMainClock = orxClock_Get(orxCLOCK_KZ_CORE);
-
-  /* Registers our input update callback to it
-   * !!IMPORTANT!! *DO NOT* handle inputs in clock callbacks that are *NOT* registered to the main clock
-   * you might miss input status updates if the user clock runs slower than the main one
-   */
-  orxClock_Register(pstMainClock, InputUpdate, orxNULL, orxMODULE_ID_MAIN, orxCLOCK_PRIORITY_NORMAL);
-
-  /* Done! */
-  return orxSTATUS_SUCCESS;
-}
-
-
-/** Update callback
- */
-void orxFASTCALL Update(const orxCLOCK_INFO *_pstClockInfo, void *_pstContext)
-{
-  orxOBJECT *pstObject;
-
-  /* *** LOG DISPLAY SECTION *** */
-
-  /* Pushes main config section */
-  orxConfig_PushSection("Main");
-
-  /* Should display log? */
-  if(orxConfig_GetBool("DisplayLog"))
-  {
-    /* Displays info in log and console */
-    orxLOG("<%s>: Time = %.3f / DT = %.3f", orxClock_GetName(orxClock_GetFromInfo(_pstClockInfo)), _pstClockInfo->fTime, _pstClockInfo->fDT);
-  }
-
-  /* Pops config section */
-  orxConfig_PopSection();
-
-  /* *** OBJECT UPDATE SECTION *** */
-
-  /* Gets object from context.
-   * The helper macro orxOBJECT() will verify if the pointer we want to cast
-   * is really an orxOBJECT.
-   */
-  pstObject = orxOBJECT(_pstContext);
-
-  /* Rotates it according to elapsed time (complete rotation every 2 seconds) */
-  orxObject_SetRotation(pstObject, orxMATH_KF_PI * _pstClockInfo->fTime);
-}
-
-/** Input update callback
- */
-void orxFASTCALL InputUpdate(const orxCLOCK_INFO *_pstClockInfo, void *_pstContext)
-{
-  orxCLOCK *pstClock;
-
-  /* *** LOG DISPLAY SECTION *** */
-
-  /* Pushes main config section */
-  orxConfig_PushSection("Main");
-
-  /* Is log input newly active? */
-  if(orxInput_HasBeenActivated("Log"))
-  {
-    /* Toggles logging */
-    orxConfig_SetBool("DisplayLog", !orxConfig_GetBool("DisplayLog"));
-  }
-
-  /* Pops config section */
-  orxConfig_PopSection();
-
-  /* *** CLOCK TIME STRETCHING SECTION *** */
-
-  /* Finds clock1.
-   * We could have stored the clock at creation, of course, but this is done here for didactic purpose. */
-  pstClock = orxClock_Get("Clock1");
-
-  /* Success? */
-  if(pstClock)
-  {
-    /* Is faster input active? */
-    if(orxInput_IsActive("Faster"))
-    {
-      /* Makes this clock go four time faster */
-      orxClock_SetModifier(pstClock, orxCLOCK_MOD_TYPE_MULTIPLY, orx2F(4.0f));
-    }
-    /* Is slower input active? */
-    else if(orxInput_IsActive("Slower"))
-    {
-      /* Makes this clock go four time slower */
-      orxClock_SetModifier(pstClock, orxCLOCK_MOD_TYPE_MULTIPLY, orx2F(0.25f));
-    }
-    /* Is normal input active? */
-    else if(orxInput_IsActive("Normal"))
-    {
-      /* Removes modifier from this clock */
-      orxClock_SetModifier(pstClock, orxCLOCK_MOD_TYPE_NONE, orxFLOAT_0);
-    }
-  }
-}
-
-
-
-/** Run function
- */
-orxSTATUS orxFASTCALL Run()
-{
-  orxSTATUS eResult = orxSTATUS_SUCCESS;
-
-  /* Should quit? */
-  if(orxInput_IsActive("Quit"))
-  {
-    /* Updates result */
-    eResult = orxSTATUS_FAILURE;
-  }
-
-  /* Done! */
-  return eResult;
-}
-
-/** Exit function
- */
-void orxFASTCALL Exit()
-{
-  /* We're a bit lazy here so we let orx clean all our mess! :) */
-}
-
-/** Main function
- */
-int main(int argc, char **argv)
-{
-  /* Executes a new instance of tutorial */
-  orx_Execute(argc, argv, Init, Run, Exit);
-
-  return EXIT_SUCCESS;
-}
-]#
