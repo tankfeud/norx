7ecb682828ec96fcf2d8e77b39c70b61945dda45
jonas <jonas.ohrn@cygni.se>
2021-10-24 23:27:47 +0200

    Generated new htmldocs.

--- generatedocs.sh
+++ generatedocs.sh
@@ -1,5 +1,5 @@
 # Generate HTML docs
-CHECK=`nim --version | grep "Version 1.3."`
+CHECK=`echo "Nim Version 1.4.44" | egrep "Version 1\.[3-9]\."`
 if [ -z "$CHECK" ]
 then
   echo "Need at least Nim version 1.3.x for this to work"
