9b6d43775e66d835c6978c4c878080b6c85d654d
jonas <jonas.ohrn@cygni.se>
2022-01-30 22:08:03 +0100

    Reverted change of build number. Norx 0.5.0 headers are made from Orx headers of last commit on branch "b-release" not tag "1.12" which has a build nr 'git rev-list --count HEAD' -> 5455.

--- src/norx/build.nim
+++ src/norx/build.nim
@@ -1,3 +1,3 @@
 # Orx 1.12 branch b-release
 # ORX_VERSION_BUILD (__orxVERSION_BUILD__) is created by Orx setup script as output from 'git rev-list --count HEAD' and written to orxBuild.h
-const ORX_VERSION_BUILD* = 5440
+const ORX_VERSION_BUILD* = 5455
