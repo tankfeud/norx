65be0182c307bb692be0c8b6ab3269c8ddd28f10
Jonas Öhrn <jonas.ohrn@cygni.se>
2024-12-02 20:50:17 +0100

    Fixed comment in build.nim

--- src/norx/build.nim
+++ src/norx/build.nim
@@ -1,3 +1,3 @@
-# Orx 1.12 branch b-release
+# Orx 1.12 git tag 1.12
 # ORX_VERSION_BUILD (__orxVERSION_BUILD__) is created by Orx setup script as output from 'git rev-list --count HEAD' and written to orxBuild.h
 const ORX_VERSION_BUILD* = 5440
